19110fb7e902cefcca3ae23b7a773ded128cd6e4
Pulagam, Kishore(kpulagam) <kpulagam@paypal.com>
2015-07-29 12:20:10 +0530

    Added exception handling for Extraction class

--- CIDashBoardServer/src/main/java/com/paypal/test/gops/admin/getdbdata/CIMongoDataManualExtraction.java
+++ CIDashBoardServer/src/main/java/com/paypal/test/gops/admin/getdbdata/CIMongoDataManualExtraction.java
@@ -1,12 +1,9 @@
 package com.paypal.test.gops.admin.getdbdata;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
-import java.util.Properties;
 import java.util.Scanner;
 
 import org.bson.Document;
@@ -16,121 +13,110 @@ import com.mongodb.ServerAddress;
 
 public class CIMongoDataManualExtraction {
 
-	private static MongoClientOptions options;
-	private static MongoClient client;
-	private static int maxConnectionsPerHost;
-	private static String dbIPAddress;
-	private static int dbPortNumber;
-	private static String dbName;
+	private static final MongoClientOptions options = MongoClientOptions
+			.builder().connectionsPerHost(100).build();
+	private static MongoClient client = new MongoClient(new ServerAddress(
+			"10.244.180.225", 27017), options);
 
 	/**
 	 * @param args
 	 * @throws IOException
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) throws IOException {
-		getDBConfigValues();
+	public static void main(String[] args) throws IOException, InterruptedException {
+		Scanner input = null;
+		
+		try{
+			
+			CIMongoDataManualExtractionDAO mongoObject = new CIMongoDataManualExtractionDAO(
+					"MetricsDB");
+			TestRunFIleHandler fileHelper = new TestRunFIleHandler();
+			fileHelper.clearScreen();
 
-		options = MongoClientOptions.builder()
-				.connectionsPerHost(maxConnectionsPerHost).build();
-		client = new MongoClient(new ServerAddress(dbIPAddress, dbPortNumber),
-				options);
+			input = new Scanner(System.in);
 
-		CIMongoDataManualExtractionDAO mongoObject = new CIMongoDataManualExtractionDAO(
-				dbName);
-		TestRunFIleHandler fileHelper = new TestRunFIleHandler();
-		fileHelper.clearScreen();
+			System.out.println("Enter the suiteName: ");
+			String suiteName = input.nextLine();
+			System.out.println("Are you Looking for last 5 run's failures");
+			String initialDecision = input.nextLine();
+			List<Document> lastFiveRunFailureList = mongoObject
+					.getLastFiveRunsFailures(client, suiteName);
+			Date currentDate = new Date();
+			File testRunFile = fileHelper.createFile();
 
-		Scanner input = new Scanner(System.in);
+			if (initialDecision.equalsIgnoreCase("Yes")) {
 
-		System.out.println("Enter the suiteName: ");
-		String suiteName = input.nextLine();
-		System.out.println("Are you Looking for last 5 run's failures");
-		String initialDecision = input.nextLine();
-		List<Document> lastFiveRunFailureList = mongoObject
-				.getLastFiveRunsFailures(client, suiteName);
-		Date currentDate = new Date();
-		File testRunFile = fileHelper.createFile();
+				if (lastFiveRunFailureList != null) {
 
-		if (initialDecision.equalsIgnoreCase("Yes")) {
+					String fileHeader = currentDate.toString();
 
-			if (lastFiveRunFailureList != null) {
+					fileHelper.writeIntoFile(lastFiveRunFailureList, fileHeader,
+							testRunFile);
 
-				String fileHeader = currentDate.toString();
-
-				fileHelper.writeIntoFile(lastFiveRunFailureList, fileHeader,
-						testRunFile);
+				}
 
 			}
 
-		}
+			else if (initialDecision.equalsIgnoreCase("No")
+					|| lastFiveRunFailureList == null) {
+				System.out.println("Are you looking for latest test run failures");
+				String secondDecision = input.nextLine();
 
-		else if (initialDecision.equalsIgnoreCase("No")
-				|| lastFiveRunFailureList == null) {
-			System.out.println("Are you looking for latest test run failures");
-			String secondDecision = input.nextLine();
-			System.out.println("Are you looking for test failures for a specific build?");
-			String thirdDecision = input.nextLine();
-			if (secondDecision.equalsIgnoreCase("Yes")) {
-				String fileHeader = currentDate.toString();
-				fileHelper.writeIntoFile(mongoObject.getFailuresForBuildId(
-						client, suiteName,
-						mongoObject.getLatestBuildID(suiteName, client)),
-						fileHeader, testRunFile);
+				if (secondDecision.equalsIgnoreCase("Yes")) {
+					String fileHeader = currentDate.toString();
+					fileHelper.writeIntoFile(mongoObject.getFailuresForBuildId(
+							client, suiteName,
+							mongoObject.getLatestBuildID(suiteName, client)),
+							fileHeader, testRunFile);
 
-			} else if (thirdDecision.equalsIgnoreCase("Yes")) {
-				System.out
-						.println("Enter the build ID for which you need the faulure for: ");
-				String userBuildId = input.nextLine();
-				System.out
-						.println("Getting the results for the given build ID: ");
-				String fileHeader = currentDate.toString();
-				fileHelper.writeIntoFile(mongoObject.getFailuresForBuildId(
-						client, suiteName, Integer.parseInt(userBuildId)),
-						fileHeader, testRunFile);
+				} else if (secondDecision.equalsIgnoreCase("No")) {
+					System.out
+							.println("Are you looking for test failures for a specific build?");
+					String thirdDecision = input.nextLine();
+					if (thirdDecision.equalsIgnoreCase("Yes")) {
+						System.out
+								.println("Enter the build ID for which you need the faulure for: ");
+						String userBuildId = input.nextLine();
+						System.out
+								.println("Getting the results for the given build ID: ");
+						String fileHeader = currentDate.toString();
+						fileHelper.writeIntoFile(mongoObject.getFailuresForBuildId(
+								client, suiteName, Integer.parseInt(userBuildId)),
+								fileHeader, testRunFile);
+
+					}
+					else{
+						System.out.println("Decide what you want to do and then come Back Again!");
+						Thread.sleep(5000);
+					}
+
+				}
+
+				else {
+					System.out.println("I guess you are entering wrong keys");
+				}
 			}
 
 			else {
 				System.out.println("I guess you are entering wrong keys");
 			}
-		}
+			
+			System.out
+			.println("Use the below link to access the file that is generated");
+			System.out.println(testRunFile.toURI().toURL());
 
-		else {
-			System.out.println("I guess you are entering wrong keys");
+			
+		}catch(Exception e){
+			
+			System.out.println("Oops! Some error occured processing your request, Apologies. Please give it a shot again!");
+			
 		}
+		
 
 		input.close();
 
 		client.close();
-		System.out
-				.println("Use the below link to access the file that is generated");
-		System.out.println(testRunFile.toURI().toURL());
-
-	}
-
-	private static void getDBConfigValues() {
-		FileInputStream ip;
-		try {
-			ip = new FileInputStream(
-					"src/main/resources/MongoConfig.properties");
-			Properties prop = new Properties();
-			prop.load(ip);
-			maxConnectionsPerHost = Integer.parseInt(prop.get(
-					"connectionsLimit").toString());
-			dbIPAddress = prop.get("dbServerIP").toString();
-			dbPortNumber = Integer
-					.parseInt(prop.get("dbPortNumber").toString());
-			dbName = prop.get("dbName").toString();
-		} catch (FileNotFoundException e) {
-			System.out.println("Unable to read data base properties file!");
-			e.printStackTrace();
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("Unable to fetch data base configuration");
-			e.printStackTrace();
-
-		}
-
+		
 	}
 
 }
